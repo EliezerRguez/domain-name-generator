7e0613c1e546528aac1f5739e09d4e6111294f9f
EliezerRguez <eliezer.rguez93@gmail.com>
2021-08-02 14:20:15 +0000

    añadida extension .org

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("btn").addEventListener("click", () => {
+    document.getElementById("name").innerHTML = domainName();
+  });
 };
+
+let pronoun = ["the", "this", "that"];
+let adj = ["magnificent", "little", "amazing"];
+let noun = ["cat", "dog", "frog"];
+let extension = [".com", ".es", ".org"];
+
+function domainName() {
+  let url = "";
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < extension.length; l++) {
+          url += pronoun[i] + adj[j] + noun[k] + extension[l] + "<br>";
+        }
+      }
+    }
+  }
+  return url;
+}
